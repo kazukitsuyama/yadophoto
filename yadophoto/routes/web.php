51cba7b9c5a92f6296d1c9bf149b2a0744b6d6ea
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-09-11 21:57:43 +0900

    プロフィール編集機能

--- yadophoto/routes/web.php
+++ yadophoto/routes/web.php
@@ -2,4 +2,6 @@
 
 Auth::routes();
 Route::get('/', 'PostsController@index');
+Route::get('/users/edit', 'UsersController@edit');
+Route::post('/users/update', 'UsersController@update');
 Route::get('/users/{user_id}', 'UsersController@show');
