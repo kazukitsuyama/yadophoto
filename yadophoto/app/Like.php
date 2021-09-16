cd4ee960119a198327a2012c978da87db77dadc4
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-09-16 22:11:32 +0900

    リレーション設定

--- yadophoto/app/Like.php
+++ yadophoto/app/Like.php
@@ -6,5 +6,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Like extends Model
 {
-    //
+    Public function user()
+  {
+    return $this->belongsTo('App\User');
+  }
+
+  Public function post()
+  {
+    return $this->belongsTo('App\Post');
+  }
 }
--- yadophoto/app/Post.php
+++ yadophoto/app/Post.php
@@ -10,4 +10,9 @@ class Post extends Model
     {
         return $this->belongsTo('APP\User');
     }
+
+    public function likes()
+    {
+      return $this->hasMany('App\Like');
+    }
 }
