c797ef22cfc5df130166f0d998205fbbb216bbe7
kazukitsuyama <sh.a.ma.nn.13.kk@gmail.com>
2021-09-16 19:26:35 +0900

    投稿機能の実装

--- yadophoto/routes/web.php
+++ yadophoto/routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
-Auth::routes();
+
 Route::get('/', 'PostsController@index');
+Auth::routes();
+// Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/home', 'PostsController@index');
+Route::get('/home', 'PostsController@index');
 Route::get('/users/edit', 'UsersController@edit');
 Route::post('/users/update', 'UsersController@update');
 Route::get('/users/{user_id}', 'UsersController@show');
+Route::get('/posts/new', 'PostsController@new')->name('new');
+Route::post('/posts','PostsController@store');
